94d8801ea6c13090274ef2a975c4ca5821e337c8
Jon Nelson <jsnelson725@gmail.com>
2015-02-13 19:20:44 -0700

    Fixed connection strings and data contexts
    web.config and some connectionstrings changed to allow connecting to
    localhost dbs

--- BBBoss_v.2/BBBoss/Controllers/ConnectionController.cs
+++ BBBoss_v.2/BBBoss/Controllers/ConnectionController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using BBBoss.Models;
+using BBBoss.DataContexts;
 using System.Data.SqlClient;
 using System.Data.Linq.SqlClient;
 using LinqToSqlShared;
@@ -14,53 +15,18 @@ namespace BBBoss.Controllers
     {
         //
         // GET: /Checkout/
-        public string GetConnectionString()
+        public BBBoss_SQLDataContext CreateConnectionString()
         {
-            return System.Configuration.ConfigurationManager.ConnectionStrings["DefaultConnection"].ConnectionString;
-        }
-
-        public ActionResult Phones()
-        {
-            return View();
-        }
-
-        //public void test(string shtuff)
-        //{
-        //    var connectionstring = GetConnectionString();
-        //    BBBoss_SQLDataContext conn = new BBBoss_SQLDataContext();
             
-            
-        //}
-
-        [HttpPost]
-        public ActionResult test(string testshtuff)
-        {
-            try
+            BBBoss_SQLDataContext dbconnect = new BBBoss_SQLDataContext();
+            if (dbconnect == null)
             {
-                if (ModelState.IsValid)
-                {
-                    //Create Connection
-                    BBBoss_SQLDataContext _conn = new BBBoss_SQLDataContext();
-                
-                    //Create db object for insert & fill data
-                    test _testshtuff = new test();
-                    _testshtuff.shtuff = testshtuff;
-     
-                    //Close DB
-                    _conn.tests.InsertOnSubmit(_testshtuff);
-                    _conn.SubmitChanges();
-                
-                }
-            }
-            catch (Exception)
-            {
-                
-                throw;
+                dbconnect = new BBBoss_SQLDataContext(global::System.Configuration.ConfigurationManager.ConnectionStrings["LocalHostConnection"].ConnectionString);
             }
             
-
-            // If we got this far, something failed, redisplay form
-            return View("Carriers");
+            return dbconnect;
         }
+
+        
     }
 }
