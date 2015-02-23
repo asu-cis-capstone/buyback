a4c4b1c5c86418ceabadc8c2a13eab1dbbe3007c
Jonscotty <jsnelson725@gmail.com>
2015-02-22 17:43:11 -0700

    BuyBackBoss Redo
    new html code structure for site

---
+++ BuyBack_Alt/models/iphone.php
@@ -0,0 +1,64 @@
+<!DOCTYPE html>
+
+<html>
+	<head>
+	<!-- Meta tag -->
+	<meta charset="utf-8"/>
+	<meta http-equiv="content-type" content="text/html;charset=utf-8" />
+
+	<!-- Link tag for CSS -->
+	<link type="text/css" rel="stylesheet" href="../content/BBB_home.css" />
+
+	<!-- JavaScript Tags -->
+
+	<!-- Web Page Title -->
+	<title>BuyBackBoss Home - iPhone Models</title>
+
+	</head>
+	
+	<body>
+	
+		<!-- Header and Navigation Bar -->
+	    <header>
+            <div class="content-wrapper">
+                <div>
+                    <nav class="navbar-fixed-top">
+                            <a class="logo-desktop" href="#"><img src="images/Buyback-Boss-Logo.png" alt="Sell and Buy Used iPhone" /></a> 
+                        <ul class="nav nav-tabs" id="menu">
+                            <li><a href="how/">HOW IT WORKS</a></li>
+                            <li><a href="reviews/">PRESS & REVIEWS</a></li>
+                            <li><a href="support/">SUPPORT</a></li>
+                            <li><a href="blog/">BLOG</a></li>
+                        </ul>
+                        <button type="button" class="cart" onclick="">
+                            CART
+                        </button>
+                    </nav>
+                </div>
+			</div>	
+        </header>
+		
+		<!-- Options and Selections -->
+		<div id="option-container">
+			<div>
+				
+			</div>
+			<div id="phones">
+				<p>
+					<a href="models/samsung.php" ><img src="images/iPhone-6-wireframe.jpg" id="iphone" /></a>
+					<a href="models/iphone.php" ><img src="images/iPhone-6-wireframe.jpg" id="samsung"/></a>
+				</p>
+				
+			</div>
+		</div>
+		
+		<!-- Footer and Copyright -->
+        <footer>
+            <div>
+                <div class="float-left">
+                    <p>&copy; 2015 - Buyback Boss</p>
+                </div>
+            </div>
+        </footer>
+	</body>
+</html>
