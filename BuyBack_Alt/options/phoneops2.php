47509cdb621a1b6da73f0d21734838f7cdafc51d
JTMonaghan <jtm3www@yahoo.com>
2015-02-23 21:10:04 -0700

    2/23/15
    PhoneOps2 Additions

---
+++ BuyBack_Alt/options/phoneops2.php
@@ -0,0 +1,167 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8" />
+        <title>Sell iPhone</title>
+        <link href="../images/Buyback-Boss-Logo.jpg" rel="shortcut icon" type="image/x-icon" />
+        <meta name="viewport" content="width=device-width" />
+        <link href="../content/BBB_phoneops2.css" rel="stylesheet"/>
+
+        <script src="../scripts/modernizr-2.5.3.js"></script>
+
+    </head>
+    <body>
+        <header>
+            <div class="content-wrapper">
+                <div>
+                    <nav class="navbar-fixed-top">
+                            <a class="logo-desktop" href="#"><img src="../images/Buyback-Boss-Logo.png" alt="Sell and Buy Used iPhone" /></a> 
+                        <ul class="nav nav-tabs" role="tablist" id="menu">
+                            <li><a href="../how/">HOW IT WORKS</a></li>
+                            <li><a href="../reviews/">PRESS &amp; REVIEWS</a></li>
+                            <li><a href="../support/">SUPPORT</a></li>
+                            <li><a href="../blog">BLOG</a></li>
+                            </ul>
+                        <button type="button" class="cart" onclick="">
+                            CART
+                         </button>
+                    </nav>
+                </div>
+            </div>
+        </header>
+        <div id="body">
+
+            
+            <div id="content-container">
+                
+    
+            <p>
+                
+            </p>
+        </div>
+    </section>
+
+                
+
+
+    <head>
+        <meta charset="utf-8" />
+        <title>Sell iPhone</title>
+        <link href="../content/BBB_phoneops2.css" rel="stylesheet"/>
+
+        <script src="../scripts/jquery-ui-1.8.20.js"></script>
+
+        <script src="../scripts/jquery-2.1.3.js"></script>
+	<script src="../scripts/bootstrap.js"></script>
+
+
+    </head>
+
+    
+    <body>
+        
+        <div class="progress">
+            <div class="progress-bar progress-bar-striped active" role="progressbar" aria-valuenow="40" aria-valuemin="0" aria-valuemax="100" style="width:75%">
+            75% complete!
+            </div>
+        </div>
+
+        <div id="phoneCheckout">
+            
+                <img src="../images/iPhone-6-wireframe.jpg" id="img_iPhone_checkout"/>
+            
+        
+        <div class = "network">
+            What condition is the device in?
+            <span class="help">
+                <button type="button" class="btn btn-default" data-container="body" data-toggle="popover" data-placement="right" data-content="Help with condition.">
+                ?
+                </button>
+            </span>
+            <div class="btn-group" data-toggle="buttons">
+                <label class = "btn btn-primary">
+                    <input type = "radio" name="condition" id="mint">MINT
+                </label>
+                <label class="btn btn-primary active">
+                    <input type="radio" name="condtion" id="good">GOOD
+                </label>
+                <label class="btn btn-primary">
+                    <input type="radio" name="condition" id="cracked">CRACKED
+                </label>
+                <label class="btn btn-primary">
+                    <input type="radio" name="condition" id="damaged">DAMAGED
+                </label>
+                <label class="btn btn-primary">
+                    <input type="radio" name="condition" id="broken">BROKEN
+                </label>
+            </div>
+           </div>
+
+           <span class="offerstatement">
+               Your offer is:
+           </span>
+
+           <span class="whybuyback">
+               Why Buyback Boss?
+           </span>
+            
+           <span class="whybuybackpoint1">
+               Prices Guaranteed for 30 days
+           </span>
+
+           <span class="whybuybackpoint2">
+               Same-day Payment
+           </span>
+
+           <span class="whybuybackpoint3">
+               Excellent Customer Service
+           </span>
+
+           <span class="checkoutButton">
+               <button class="btn btn-default round" type="submit" value="checkout">Checkout</button>
+           </span>
+
+           <span class="offerAmount">
+               $355
+           </span>
+
+           <span class="conditionDescription">
+               If <b>ALL</b> of the following are true:<br />
+               -Fully Functional<br />
+               -All buttons work<br />
+               -No chips or major scratches<br />
+               -Never exposed to liquid<br />
+           </span>
+
+           <span class="conditionIndicator">
+
+           </span>
+
+        </div>
+
+        
+        
+        
+    </body>
+       
+
+                
+
+
+            </div>           
+        </div>
+        <footer>
+            <div class="content-wrapper">
+                <div class="float-left">
+                    <p>&copy; 2015 - Buyback Boss</p>
+                </div>
+            </div>
+        </footer>
+
+        <script src="/Scripts/jquery-2.1.3.js"></script>
+
+        
+        
+   
+    </body>
+</html>
