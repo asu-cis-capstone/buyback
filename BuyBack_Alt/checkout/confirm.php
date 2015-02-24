59f93a7eb69bdc220d1785ca7c4995f1c10e1d79
Jon Nelson <jsnelson725@gmail.com>
2015-02-23 20:21:51 -0700

    Confirm page and css
    confirm.php

---
+++ BuyBack_Alt/checkout/confirm.php
@@ -0,0 +1,59 @@
+<!DOCTYPE html>
+
+<html>
+	<head>
+	<!-- Meta tag -->
+	<meta charset="utf-8"/>
+	<meta http-equiv="content-type" content="text/html;charset=utf-8" />
+
+	<!-- Link tag for CSS -->
+	<link type="text/css" rel="stylesheet" href="../content/BBB_header.css" />
+	<link type="text/css" rel="stylesheet" href="../content/BBB_confirm.css" />
+
+	<!-- JavaScript Tags -->
+
+	<!-- Web Page Title -->
+	<title>BuyBackBoss Checkout Confirm</title>
+
+	</head>
+	
+	<body>
+	    <header>
+            <div class="content-wrapper">
+                <div>
+                    <nav class="navbar-fixed-top">
+                            <a class="logo-desktop" href="../"><img src="../images/Buyback-Boss-Logo.png" alt="Sell and Buy Used iPhone" /></a> 
+                        <ul class="nav nav-tabs" id="menu">
+                            <li><a href="../how/">HOW IT WORKS</a></li>
+                            <li><a href="../reviews/">PRESS & REVIEWS</a></li>
+                            <li><a href="../support/">SUPPORT</a></li>
+                            <li><a href="../blog/">BLOG</a></li>
+                        </ul>
+                        <button type="button" class="cart" onclick="">
+                            CART
+                        </button>
+                    </nav>
+                </div>
+			</div>	
+        </header>
+		<div id="message-container">
+			<div id="message">
+				<p>Your order is being processed!</p>
+				<p>An email has been sent to the address with your receipt and confirmation</p>
+				<p>Thanks for choosing us! We greatly appreciate it.</p>
+			</div>
+			<div>
+				<button type="button" id="return" onclick="window.navigate('../index.html')">
+					HOME
+				</button>
+			</div>
+		</div>
+        <footer>
+            <div>
+                <div class="float-left">
+                    <p>&copy; 2015 - Buyback Boss</p>
+                </div>
+            </div>
+        </footer>
+	</body>
+</html>
