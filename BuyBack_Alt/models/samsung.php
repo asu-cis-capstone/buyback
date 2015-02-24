e8ef6c35cd5216a8899180ab1dc8170e5b00b951
Msaalabi <msaalabi@asu.edu>
2015-02-23 17:08:52 -0700

    Updated samsung.php
    Same updates as iphone.php

--- BuyBack_Alt/models/samsung.php
+++ BuyBack_Alt/models/samsung.php
@@ -8,6 +8,8 @@
 
 	<!-- Link tag for CSS -->
 	<link type="text/css" rel="stylesheet" href="../content/BBB_home.css" />
+	<link type="text/css" rel="stylesheet" href="../content/BBB_header.css" />
+	<link type="text/css" rel="stylesheet" href="../content/BBB_samsungmodel.css" />
 
 	<!-- JavaScript Tags -->
 
@@ -22,7 +24,7 @@
             <div class="content-wrapper">
                 <div>
                     <nav class="navbar-fixed-top">
-                            <a class="logo-desktop" href="#"><img src="images/Buyback-Boss-Logo.png" alt="Sell and Buy Used iPhone" /></a> 
+                            <a class="logo-desktop" href="#"><img src="../images/Buyback-Boss-Logo.png" alt="Sell and Buy Used iPhone" /></a> 
                         <ul class="nav nav-tabs" id="menu">
                             <li><a href="how/">HOW IT WORKS</a></li>
                             <li><a href="reviews/">PRESS & REVIEWS</a></li>
@@ -37,13 +39,33 @@
 			</div>	
         </header>
 		
-		<div id="phones">
-			<p>
-				<a href="models/samsung.php" ><img src="images/iPhone-6-wireframe.jpg" id="iphone" /></a>
-				<a href="models/iphone.php" ><img src="images/iPhone-6-wireframe.jpg" id="samsung"/></a>
-			</p>
-			
-		</div>			
+		<div class="progress">
+		<div class="progress-bar progress-bar-striped active" role="progressbar"
+			aria-valuenow="40" aria-valuemin="0" aria-valuemax="100" style="width:25%">
+			25% complete!
+		</div>
+		</div>
+	
+		<div id="phoneImage">
+            <img src="../images/iPhone-6-wireframe.jpg" id="small_phone_left"/>
+		</div>
+		
+		<!-- Options and Selections -->
+			<div id="phones">
+				<p>
+					<form action="../options/phoneops1.php">
+					<input type="submit" id="Samsung_Submit" value="Galaxy s5 Mini">
+					<input type="submit" id="Samsung_Submit" value="Galaxy s5 Sport">
+					<input type="submit" id="Samsung_Submit" value="Galaxy s5">
+					<input type="submit" id="Samsung_Submit" value="Galaxy s4 Mini">
+					<input type="submit" id="Samsung_Submit" value="Galaxy s4">
+					<input type="submit" id="Samsung_Submit" value="Galaxy Note 4">
+					<input type="submit" id="Samsung_Submit" value="Galaxy Note Edge">
+					</form>				
+				</p>		
+			</div>
+            
+		<!-- Footer and Copyright -->			
         <footer>
             <div>
                 <div class="float-left">
