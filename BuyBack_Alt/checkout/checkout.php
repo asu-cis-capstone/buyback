e235c8a071a2625a123e278c81dc8367cf4996f8
Carrie Dougher <cmdough1@asu.edu>
2015-03-06 13:03:21 -0700

    Updated checkout page
    DB posting/reading on my end

--- BuyBack_Alt/checkout/checkout.php
+++ BuyBack_Alt/checkout/checkout.php
@@ -1,135 +1,109 @@
 <!DOCTYPE html>
 
-<html>
-	<head> 
-	<!-- Meta tag -->
-	<meta charset="utf-8"/>
-	<meta http-equiv="content-type" content="text/html;charset=utf-8" />
 
-	<!-- Link tag for CSS -->
-	<link type="text/css" rel="stylesheet" href="../content/BBB_checkout.css" />
-	<link type="text/css" rel="stylesheet" href="../content/BBB_header.css" />
-	<!--<link type="text/css" rel="stylesheet" href="../content/bootstrap.css" />-->
+
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="">
+   
+
+    <title>Buyback Boss</title>
+
+    <!-- Bootstrap core CSS -->
+    <link href="../content/bootstrap.min.css" rel="stylesheet">
 	
-	
-	<link href="../images/Buyback-Boss-Logo.jpg" rel="shortcut icon" type="image/x-icon" />
+	<!--Fonts-->
+	<link href='http://fonts.googleapis.com/css?family=Lobster|Oswald:400,700|Bree+Serif|Roboto+Condensed:700italic,400,300|Open+Sans+Condensed:300,700' rel='stylesheet' type='text/css'>
 
-	<!-- JavaScript Tags -->
-        <script src="../scripts/jquery-2.1.3.js" type="text/javascript"></script>
-		<script src="../scripts/bootstrap.js" type="text/javascript"></script>
-		<script src="../scripts/main.js" type="text/javascript"></script>
+    <!-- Custom styles for this template -->
+    <link href="../content/sticky-footer-navbar.css" rel="stylesheet">
+	<link href="../content/BBB_checkout2.css" rel="stylesheet">
 
-	<!-- Web Page Title -->
-	<title>Checkout</title>
+    <!-- Just for debugging purposes. Don't actually copy these 2 lines! -->
+    <!--[if lt IE 9]><script src="../../assets/js/ie8-responsive-file-warning.js"></script><![endif]-->
+    <script src="../../assets/js/ie-emulation-modes-warning.js"></script>
 
-	</head>
-	
-	<body>
-	<div id="option">
-		<!-- Modal -->
-		<div class="modal fade" id="refer">
-		  <div class="modal-dialog">
-			<div class="modal-content">
-			  <div class="modal-header">
-				<button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
-				<h4 class="modal-title">Do you have a referral code?</h4>
-			  </div>
-			  <div class="modal-body">
-				<form id="referral" action="process.php" method="post">
-					<!-- Referral Code -->	
-					<label for="refcode">Referral/Coupon Code</label> 
-					<input type="text" class="form-control" id="refcode" name="refcode"
-					pattern="[a-z0-9]{6,6}"
-					title="Invalid referral code!"/>
-			
-					<!-- Validate Button for Ref Code -->	
-					<span class="validate">
-					<input class="btn btn-success" type="submit" id="validate" value="Validate Code" 
-					onclick="closemodal(); location.href='process.php';"></button>
-					</span>
-				</form>
-			  </div>
-			</div><!-- /.modal-content -->
-		  </div><!-- /.modal-dialog -->
-		</div><!-- /.modal -->
-	</div>
-	    <!-- Header and Navigation Bar -->
-	    <header>
-            <div class="content-wrapper">
-                <div>
-                    <nav class="navbar-fixed-top">
-                            <a class="logo-desktop" href="#"><img src="../images/Buyback-Boss-Logo.png" alt="Sell and Buy Used iPhone" /></a> 
-                        <ul class="nav nav-tabs" id="menu">
-                            <li><a href="../how/">HOW IT WORKS</a></li>
-                            <li><a href="../reviews/">PRESS & REVIEWS</a></li>
-                            <li><a href="../support/">SUPPORT</a></li>
-                            <li><a href="../blog/">BLOG</a></li>
-                        </ul>
-                        <button type="button" class="cart" onclick="">
-                            CART
-                        </button>
-                    </nav>
-                </div>
-			</div>	
-        </header>
+    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+  </head>
 
-		
-		<!-- What your selling, option to go back, option to add another device
-		<div id="youareselling">
-			<div class="leftarrow">
-				<a href="#"><img src="../images/leftarrow.png" id="left_arrow"/></a>
-			</div>
-			
-			<div class="sellingpic">
-				<img src="../images/youareselling.png" id="You_Are_Selling"/>
-			</div>
-			
-		<!--Add Another Device Button...?-->
-		<!---<div id="addanother">
-			<button type="add" class="add_btn">Add Another Device</button>
-		</div>
-		</div>-->
-		<div id="title">
-			Checkout
-		</div>
-		
-		<div id="form">
-			
-			<!-- FORM -->
-			<form action="confirm.php" method="post" class="form-inline">	
-			
-			<!-- Choose Payment Method -->
-				<div class="paymentmethod">
-					<p class="label">Payment Method:</p>
-					<div class="btn-group" data-toggle="buttons">
-						<label class="btn btn-primary active">
-							<input type="radio" name="payment" id="paypal"><img src="../images/paypal.png" alt="paypal" />
-						</label>
-						<label class="btn btn-primary">
-							<input type="radio" name="payment" id="check"><img src="../images/check.png" alt="check" /><span class="btntext">Check</span>
-						</label>
+  <body>
+
+    <!-- Fixed navbar -->
+    <nav class="navbar navbar-default navbar-fixed-top">
+      <div class="container">
+        <div class="navbar-header">
+          <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+            <span class="sr-only">Toggle navigation</span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+          </button>
+          <a class="navbar-brand" href="#"><img id="navlogo" src="../images/Buyback-Boss-Logo.png"  alt="logo"></a>
+        </div>
+        <div id="navbar" class="collapse navbar-collapse">
+          <ul class="nav navbar-nav">
+			<li><a href="../index.php">HOME</a></li>
+            <li><a href="../index.php#home">HOW IT WORKS</a></li>
+            <li><a href="#press">PRESS & REVIEWS</a></li>
+            <li class="SUPPORT">
+              <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">SUPPORT<span class="caret"></span></a>
+              <ul class="dropdown-menu" role="menu">
+                <li><a href="#">Track Your Order</a></li>
+                <li><a href="#">FAQ</a></li>
+                <li class="divider"></li>
+                <li class="dropdown-header">Contact</li>
+                <li><a href="#">24/7 Chat Support</a></li>
+                <li><a href="#">Contact Form</a></li></ul>
+				<li><a href="#">BLOG</a></li></li>
+              </ul>
+			  <ul class="nav navbar-nav navbar-right">
+					<li><a href="#cart">CART<span class="sr-only">(current)</span></a></li>
+			  </ul>
+        </div><!--/.nav-collapse -->
+      </div>
+    </nav>
+
+    <!-- Begin page content -->
+    <div class="container">
+	  <div class="page-header">
+	  		<div id="steps">
+				<div class="stepwizard">
+					<div class="stepwizard-row">
+						<div class="stepwizard-step">
+							<button type="button" class="btn btn-default btn-circle" onclick="location.href='../models/models.php'">1</button>
+							<p>Model</p>
+						</div>
+						<div class="stepwizard-step">
+							<button type="button" class="btn btn-default btn-circle" onclick="../options/phoneops11.php">2</button>
+							<p>Options</p>
+						</div>
+						<div class="stepwizard-step">
+							<button type="button" class="btn btn-default btn-circle" onclick="../options/phoneops22.php">3</button>
+							<p>Quote</p>
+						</div> 
+						<div class="stepwizard-step">
+							<button type="button" class="btn btn-primary btn-circle" onclick="#">4</button>
+							<p>Checkout</p>
+						</div>
 					</div>
 				</div>
-			
-			<!-- Choose Shipping Method -->
-			<div class="shippingmethod">
-				<p class="label">Shipping Method:</p>
-				<div class="btn-group" data-toggle="buttons">
-					<label class="btn btn-primary active">
-						<input type="radio" name="payment" id="paypal" value="paypal"><span class="btntext">Send me a buyback box!</span>
-					</label>
-					<label class="btn btn-primary">
-						<input type="radio" name="payment" id="check"><span class="btntext">I have my own box! Print a label now!</span>
-					</label>
-				</div>
 			</div>
-		
-			<div id="offercontainer">
+      </div>
+
+	  		<div id="offercontainer">
 				<span class="offerstatement">
 				Your offer:
 				</span>
 			<div id="offerAmount">
-				
+				test
 				<?php
 					if ($_GET["rc"] == 1)
 					{
@@ -148,14 +122,92 @@
 				
 			</div>
 		</div>
-		
-		<!-- Shipping -->
+	 <div id="forms">
+	<div class="row form-group">
+        <div class="col-xs-12">
+            <ul class="nav nav-pills nav-justified thumbnail setup-panel">
+                <li class="active"><a href="#step-1">
+                    <h4 class="list-group-item-heading">Step 1</h4>
+                    <p class="list-group-item-text">Referral &amp; Coupon Codes</p>
+                </a></li>
+                <li class="disabled"><a href="#step-2">
+                    <h4 class="list-group-item-heading">Step 2</h4>
+                    <p class="list-group-item-text">Payment &amp; Shipping Options</p>
+                </a></li>
+                <li class="disabled"><a href="#step-3">
+                    <h4 class="list-group-item-heading">Step 3</h4>
+                    <p class="list-group-item-text">Shipping &amp; Mailing Address</p>
+                </a></li>
+            </ul>
+        </div>
+	</div>
+    
+	<div class="row setup-content" id="step-1">
+        <div class="col-xs-12">
+            <div class="col-md-12 well text-center">
+                <h1>Do you have a <strong>referral</strong> or <strong>coupon</strong> code?</h1>
+					<form id="referral" action="process.php" method="post">
+					<!-- Referral Code -->	
+					<input type="text" class="form-control" id="refcode" name="refcode"
+					pattern="[a-z0-9]{6,6}"
+					title="Invalid referral code!"/>
+			
+					<!-- Validate Button for Ref Code -->	
+					<span class="validate">
+					<input class="btn btn-success" type="submit" id="validate" value="Validate Code" 
+					onclick="closemodal(); location.href='process.php';"></button>
+					</span>
+				</form>
+                <br/><button id="activate-step-2" class="btn btn-primary btn-lg">Next</button>
+            </div>
+        </div>
+    </div>
+	
+	<!-- FORM -->
+	<form action="confirm.php" method="post" class="form-inline">
+    <div class="row setup-content" id="step-2">
+        <div class="col-xs-12">
+            <div class="col-md-12 well">
+                <h1 class="text-center"> How would you like us to <strong>pay</strong> you?</h1>
+				<!-- Choose Payment Method -->
+				<div class="paymentmethod">
+					<div class="btn-group" data-toggle="buttons">
+						<label class="btn btn-primary active">
+							<input type="radio" name="payment" value="paypal" id="paypal"><img id="paypalimg" src="../images/paypal.png" alt="paypal" />
+						</label>
+						<label class="btn btn-primary">
+							<input type="radio" name="payment" value="check" id="check"><img id="checkimg" src="../images/check.png" alt="check" /><br/><span class="btntext1">Check</span>
+						</label>
+					</div>
+				</div>
+			
+			<h1 class="text-center"> How would you like to<strong> ship </strong>your device?<br/><span class="sub">We pay for ALL shipping! It's FREE for you!</span></h1>
+			<!-- Choose Shipping Method -->
+			<div class="shippingmethod">
+				<div class="btn-group" data-toggle="buttons">
+					<label class="btn btn-primary active">
+						<input type="radio" name="payment" id="paypal" value="paypal"><span class="btntext">Send me a <br/>buyback box!</span>
+						
+					</label>
+					<label class="btn btn-primary">
+						<input type="radio" name="payment" id="check"><span class="btntext">I have my own box!<br/> Print a shipping label now!</span>
+					</label>
+				</div>
+			</div>
+				<button id="activate-step-3" class="btn btn-primary btn-lg">Next</button>
+            </div>
+        </div>
+    </div>
+    <div class="row setup-content" id="step-3">
+        <div class="col-xs-12">
+            <div class="col-md-12 well">
+                <h1 class="text-center">Address Information</h1>
+			<!-- Shipping -->
 			<div id="shipinfo">
-			<p class="label">Shipping Information:</p>
 			<div class="form-group">
 			
 			<!-- First Name -->
-		`		<input type="text" class="form-control" id="firstname" name="firstname"
+		<input type="text" class="form-control" id="firstname" name="firstname"
 				required autofocus
 				pattern="[a-zA-Z-' ]{2,30}"
 				title="Name: 2-30 chars, u/l case letters and - or ' only!"
@@ -168,6 +220,7 @@
 				title="Name: 2-30 chars, u/l case letters and - or ' only!"
 				placeholder="Last Name" />
 				
+				<br/>
 			<!-- Email -->	
 				<input type="email" class="form-control" id="email" name="email"
 				required
@@ -182,6 +235,7 @@
 				title="US Based Phone Number 10 numbers exactly"
 				placeholder="Phone Number"/>
 				
+				<br/>
 			<!-- Street Address -->	
 				<input type="text" class="form-control" id="streetaddress" name="streetaddress"
 				required
@@ -194,6 +248,7 @@
 				maxlength="20"
 				placeholder="Apt. Unit P.O. Box etc..." />
 				
+				<br/>
 			<!-- City -->
 				<input type="text" class="form-control" id="city" name="city"
 				required
@@ -279,21 +334,36 @@
 				</select>
 				
 				
-			</div>
+			</div><br/>
 			<span class="checkoutButton">
                <button class="btn btn-primary" type="submit" value="checkout">Submit</button>
 			</span>
-			</form>
 			
 			
-		</div></div>
-		
-        <footer>
-            <div>
-                <div class="float-left">
-                    <p>&copy; 2015 - Buyback Boss</p>
-                </div>
+		</div>
+				
             </div>
-        </footer>
-	</body>
+        </div>
+    </div>
+	
+</form></div>
+</div>
+
+
+
+    <footer class="footer">
+      <div class="container">
+        <p class="text-muted">Buyback Boss Information</p>
+      </div>
+    </footer>
+
+
+    <!-- Bootstrap core JavaScript
+    ================================================== -->
+    <!-- Placed at the end of the document so the pages load faster -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
+    <script src="../scripts/bootstrap.js"></script>
+	<script src="../scripts/main.js"></script>
+
+  </body>
 </html>
